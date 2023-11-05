1ba7cfb31e99392afe5788d39aaf0f5cf2e22279
Diego Yann <diego.yalmeida@hsl.org.br>
2023-11-05 13:31:30 -0300

    Corrigido o erro na conexao ao excluir

--- Locadora/Locadora/Formularios/FrmCadFuncionarios.vb
+++ Locadora/Locadora/Formularios/FrmCadFuncionarios.vb
@@ -103,39 +103,45 @@ Public Class FrmCadFuncionarios
     End Function
 
     Private Sub BtExcluirClick(sender As Object, e As EventArgs) Handles btExcluir.Click
-        Dim con As New Connection()
-        Dim cpf As String = txtCpfExcluir.Text
-        Dim login As String = txtUsuarioExcluir.Text
-        If Not ValidarCampos() Then
-            Return
+    Dim con As New Connection()
+    Dim cpf As String = txtCpfExcluir.Text
+    Dim login As String = txtUsuarioExcluir.Text
+
+    If Not ValidarCampos() Then
+        Return
+    End If
+
+    Try
+        
+        con.OpenConnection()
+
+        If MessageBox.Show("Tem certeza de que deseja excluir este funcionário?", "Confirmação de exclusão", MessageBoxButtons.YesNo, MessageBoxIcon.Question) = DialogResult.Yes Then
+            With con
+                .Command.Parameters.Clear()
+                .Command.Parameters.Add(.InserirParametro(cpf, "@cpf", DbType.String))
+                .Command.Parameters.Add(.InserirParametro(login, "@login", DbType.String))
+
+                .Command.CommandText = "DELETE FROM TbFuncionarios WHERE Cpf = @cpf and Login = @login"
+                Dim rowsAffected As Integer = .Command.ExecuteNonQuery()
+
+                If rowsAffected > 0 Then
+                    MessageBox.Show("Funcionário excluído com sucesso.", "Sucesso", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                    ' Limpa os campos após a exclusão bem-sucedida
+                    txtCpfExcluir.Clear()
+                    txtUsuarioExcluir.Clear()
+                Else
+                    MessageBox.Show("Nenhum funcionário foi excluído. Verifique o CPF e tente novamente.", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+                End If
+            End With
         End If
+    Catch ex As Exception
+        MessageBox.Show("Ocorreu um erro ao excluir o funcionário. Detalhes: " & ex.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error)
+    Finally
+        
+        con.CloseConnection()
+    End Try
+End Sub
 
-        Try
-
-            If MessageBox.Show("Tem certeza de que deseja excluir este funcionário?", "Confirmação de exclusão", MessageBoxButtons.YesNo, MessageBoxIcon.Question) = DialogResult.Yes Then
-                With con
-                    .Command.Parameters.Clear()
-                    .Command.Parameters.Add(.InserirParametro(cpf, "@cpf", DbType.String))
-                    .Command.Parameters.Add(.InserirParametro(login, "@login", DbType.String))
-
-                    .Command.CommandText = "DELETE FROM TbFuncionarios WHERE Cpf = @cpf and Login = @login"
-                    Dim rowsAffected As Integer = .Command.ExecuteNonQuery()
-
-                    If rowsAffected > 0 Then
-                        MessageBox.Show("Funcionário excluído com sucesso.", "Sucesso", MessageBoxButtons.OK, MessageBoxIcon.Information)
-                        ' Limpa os campos após a exclusão bem-sucedida
-                        txtCpfExcluir.Clear()
-                        txtUsuarioExcluir.Clear()
-                    Else
-                        MessageBox.Show("Nenhum funcionário foi excluído. Verifique o CPF e tente novamente.", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning)
-                    End If
-                End With
-            End If
-
-        Catch ex As Exception
-            MessageBox.Show("Ocorreu um erro ao excluir o funcionário. Detalhes: " & ex.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error)
-        End Try
-    End Sub
 
     Private Sub FrmCadFuncionarios_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Dim ObjFunctions = New Funcoes(Me)
